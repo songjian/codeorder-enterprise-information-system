64ec1a47d30cb80d5e4d09dd4931e1e1c083f31c
宋健 <001@codeorder.cn>
2019-06-18 16:34:29 +0800

    添加创建和删除猎头脚本

---
+++ sh/scaffold_headhunter_add.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rails g scaffold Headhunter --parent=User
+rails g scaffold_controller Admin/Headhunter
---
+++ sh/scaffold_headhunter_del.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rails d scaffold_controller Admin/Headhunter
+rails d scaffold Headhunter --parent=User
